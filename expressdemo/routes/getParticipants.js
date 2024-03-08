bf6a4085b8b8c2577394b01547ba16e18cb98ced
WHZ <zhou@zhoudeMacBook-Pro.local>
2024-03-07 19:40:57 -0800

    get meetingInfo 2

--- expressdemo/routes/getParticipants.js
+++ expressdemo/routes/getParticipants.js
@@ -1,10 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
+var jwt = require('jsonwebtoken');
+
+
+// 定义Zoom API的基本URL和认证信息
+const baseUrl = 'https://api.zoom.us/v2';
+//const zoomToken = 'YOUR_ZOOM_API_TOKEN';MZr0OfgITFOzVrlf_30lQg
+const apiKey = 'OsnJQFhURo2zOH3ettr1EA';
+const apiSecret = 'ecRkinAbmE4ZZrwySsx6DzyiO4WBVTya';
+
+const payload = {
+  iss: apiKey,
+  exp: ((new Date()).getTime() + 5000)
+};
+
+const zoomToken = jwt.sign(payload, apiSecret);
+
+console.log(zoomToken);
 
 // get Participants infornation
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.get('/', async (req, res) => {
+    try {
+        const { meetingId, password } = req.query;
+
+        
+        const zoomMeetingParticipants = await axios.get(`${baseUrl}/meeting/${meetingId}/participants`, {
+           headers: {
+              Authorization: `Bearer ${zoomToken}`
+            }
+        });
+
+        // 将 Zoom meeting 参会者信息传递给 EJS 模板
+        res.render('participants', { participants: zoomMeetingParticipants.data });
+    } catch (error) {
+        console.error('Error fetching Zoom meeting participants:', error);
+        res.status(500).send('Error fetching Zoom meeting participants');
+    }
 });
 
 module.exports = router;
+
+
 
